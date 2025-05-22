7e5d75480cc8f1b8f85df5edeebae59ca7412f75
Анна Цыгельникова <annatsygelnikova@mail.ru>
2025-05-22 18:06:45 +0000

    push

--- docker/src/database.py
+++ docker/src/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
+DB_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine('postgresql+psycopg://kubsu:kubsu@127.0.0.1:5432/kubsu', echo=True)
+engine = create_async_engine(DB_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
